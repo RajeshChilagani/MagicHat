eb976a9be333d8356538b24825a6458c96ed703e
Anvesh E <anvesh.e9@gmail.com>
2018-11-28 10:26:35 +0530

    Create Score.cs

---
+++ MagicHat/Assets/-MyScripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    public Text scoreText;
+    public int ballValue;
+    private int score;
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        Update();
+	}
+    void OnTriggerEnter2D()
+    {
+        score += ballValue;
+        Update();
+    }
+	
+	void Update()
+    {
+        scoreText.text = "Score\n" + score;
+    }
+}
